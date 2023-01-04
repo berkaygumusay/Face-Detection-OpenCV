c28ef7afd9f34c7c5915b021d88381f0bf44a446
Berkay Gümüşay <92121824+berkaygumusay@users.noreply.github.com>
2023-01-04 18:50:03 +0300

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -1,6 +1,8 @@
 import cv2 as cv 
 
-img = cv.imread('OpenCV\Photos\Aynen.jpg')
+#You can use any photo you want
+
+img = cv.imread('OpenCV\Photos\Fileame.jpg')
 cv.imshow('aynen',img)
 
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
@@ -10,12 +12,12 @@ haarCascade = cv.CascadeClassifier('OpenCV\Face Detection with Haar Cascades\haa
 
 
 
-facesRect = haarCascade.detectMultiScale(gray,scaleFactor=1.1111111,minNeighbors=1)
+facesRect = haarCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
 print(f'Number of faces = {len(facesRect)}')
 for (x,y,w,h) in facesRect:
     cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),thickness=2)
 
 
 
-cv.imshow('aynenFaces',img)
+cv.imshow('FaceDetect',img)
 cv.waitKey(0)
